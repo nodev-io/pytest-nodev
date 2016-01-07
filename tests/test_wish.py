e6cfe02ddfc043c25d07658d4c98f1dee13f515d
Alessandro Amici <alexamici@gmail.com>
2016-01-08 00:30:39 +0100

    test support for skipping tests when no wish_modules option is provided

--- tests/test_wish.py
+++ tests/test_wish.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 
+TEST_FACTORIAL = """
+def test_factorial(wish):
+    assert wish(0) == 1
+    assert wish(1) == 1
+    assert wish(21) == 51090942171709440000
+"""
+
 
 def test_generate_tests(testdir):
     """Make sure that pytest accepts our fixture."""
 
     # create a temporary pytest test module
-    testdir.makepyfile("""
-        def test_factorial(wish):
-            assert wish(0) == 1
-            assert wish(1) == 1
-            assert wish(21) == 51090942171709440000
-    """)
+    testdir.makepyfile(TEST_FACTORIAL)
 
     # run pytest with the following cmd args
     result = testdir.runpytest(
@@ -21,6 +23,26 @@ def test_generate_tests(testdir):
     # fnmatch_lines does an assertion internally
     result.stdout.fnmatch_lines([
         '*::test_factorial*math:factorial*XPASS',
+    ])
+
+    # make sure that that we get a '0' exit code for the testsuite
+    assert result.ret == 0
+
+
+def test_skip_tests(testdir):
+    """Make sure that pytest accepts our fixture."""
+
+    # create a temporary pytest test module
+    testdir.makepyfile(TEST_FACTORIAL)
+
+    # run pytest with the following cmd args
+    result = testdir.runpytest(
+        '-v',
+    )
+
+    # fnmatch_lines does an assertion internally
+    result.stdout.fnmatch_lines([
+        '*::test_factorial*wish*SKIPPED',
     ])
 
     # make sure that that we get a '0' exit code for the testsuite
