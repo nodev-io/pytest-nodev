a496f3e055648787b15683f6e98f98e406649d67
Alessandro Amici <alexamici@gmail.com>
2016-01-08 17:07:54 +0100

    Remove a partial example commited by mistake.

--- examples/test_remove_comments.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-def test_remove_comments(wish):
-    remove_comments = wish
-    assert list(remove_comments('#')) == ['']
-    assert list(remove_comments('#\n')) == ['']
-    assert list(remove_comments('# comment\nnon comment')) == ['', 'non comment']
-    assert list(remove_comments('# comment\nnon comment\n')) == ['', 'non comment']
