c2bb7f0461599cc7624b8d844be93b6912fc0b1d
Alessandro Amici <alexamici@gmail.com>
2016-01-14 21:42:54 +0100

    Complete unfinished code committed by mistake.

--- examples/test_filter_strings.py
+++ examples/test_filter_strings.py
@@ -1,5 +1,7 @@
 
-def test_filter_strings(wish):
-    accept_names = wish
-    names = ['has MARK', 'does not have']
+def test_filter_strings_basic(wish):
+    filter_strings = wish
+    input = ['has MARK', 'does not have']
+    expected_ouput = ['has MARK']
     accept_pattern = '.*MARK.*'
+    assert list(filter_strings(input, accept_pattern)) == expected_ouput
