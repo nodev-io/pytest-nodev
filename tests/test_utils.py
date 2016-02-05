372d903a5efdb0381d6ecf27f288b476b4587aa2
Alessandro Amici <alexamici@gmail.com>
2016-02-05 09:13:07 +0100

    Improve fucntional coverage of valid_name test.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -47,10 +47,18 @@ def test_generate_module_objects():
 
 
 def test_valid_name():
-    assert not utils.valid_name('math:factorial', 'a')
-    assert utils.valid_name('math:factorial', 'm')
-    assert utils.valid_name('math:factorial', '.*factorial$', 'moo')
-    assert not utils.valid_name('math:factorial', 'm', 'math')
+    assert utils.valid_name('math:factorial')
+
+    assert utils.valid_name('math:factorial', include_pattern='math')
+    assert utils.valid_name('math:factorial', include_pattern='.*:factorial$')
+    assert not utils.valid_name('math:factorial', include_pattern='abc')
+
+    assert not utils.valid_name('math:factorial', exclude_pattern='math')
+    assert not utils.valid_name('math:factorial', exclude_pattern='.*:factorial$')
+    assert utils.valid_name('math:factorial', exclude_pattern='abc')
+
+    assert utils.valid_name('math:factorial', include_pattern='m', exclude_pattern='moo')
+    assert not utils.valid_name('math:factorial', include_pattern='m', exclude_pattern='math')
 
 
 def test_generate_objects_from_modules():
