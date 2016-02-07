16316fd413477fc4146781abd9bd66c05a3e7303
Alessandro Amici <alexamici@gmail.com>
2016-02-07 14:15:38 +0100

    Clean remnants of coockiecutter in docs.

--- docs/conf.py
+++ docs/conf.py
@@ -203,7 +203,7 @@ html_static_path = ['_static']
 #html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'pytest-cookiecutterplugin_namedoc'
+htmlhelp_basename = 'pytest-wish_namedoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -225,8 +225,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'pytest-cookiecutterplugin_name.tex', u'pytest-\\{\\{cookiecutter.plugin\\_name\\}\\} Documentation',
-   u'\\{\\{cookiecutter.full\\_name\\}\\}', 'manual'),
+  (master_doc, 'pytest-wish_name.tex', u'pytest-wish Documentation',
+   u'Alessandro Amici', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -255,7 +255,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'pytest-cookiecutterplugin_name', u'pytest-wish Documentation',
+    (master_doc, 'pytest-wish_name', u'pytest-wish Documentation',
      [author], 1)
 ]
 
@@ -269,8 +269,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'pytest-cookiecutterplugin_name', u'pytest-wish Documentation',
-   author, 'pytest-cookiecutterplugin_name', 'One line description of project.',
+  (master_doc, 'pytest-wish_name', u'pytest-wish Documentation',
+   author, 'pytest-wish_name', 'One line description of project.',
    'Miscellaneous'),
 ]
 
