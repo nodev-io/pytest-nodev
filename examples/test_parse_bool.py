32a1ccc5856fd58e841a3960dbe17ceb029eb8d8
Alessandro Amici <alexamici@gmail.com>
2016-02-09 02:50:26 +0100

    Add test file use in the docs.

---
+++ examples/test_parse_bool.py
@@ -0,0 +1,8 @@
+def test_parse_bool(wish):
+    parse_bool = wish
+    assert not parse_bool('false')
+    assert not parse_bool('FALSE')
+    assert not parse_bool('0')
+    assert parse_bool('true')
+    assert parse_bool('TRUE')
+    assert parse_bool('1')
