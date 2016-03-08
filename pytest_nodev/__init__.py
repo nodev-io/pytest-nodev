fd401107692921d1540722695067fe60550ec3a0
Alessandro Amici <alexamici@gmail.com>
2016-03-08 22:48:27 +0100

    Add the pytest_nodev.search decorator for easy instrumentation.

--- pytest_nodev/__init__.py
+++ pytest_nodev/__init__.py
@@ -2,3 +2,20 @@
 #
 # Copyright (c) 2015-2016 Alessandro Amici
 #
+
+# python 2 support via python-future
+from __future__ import absolute_import, unicode_literals
+
+import inspect
+
+
+def search(target_name):
+    def wish_decorator(test_func):
+        def wish_wrapper(wish, monkeypatch, *args, **kwargs):
+            if '.' in target_name:
+                monkeypatch.setattr(target_name, wish, raising=False)
+            else:
+                monkeypatch.setattr(inspect.getmodule(test_func), target_name, wish, raising=False)
+            return test_func(*args, **kwargs)
+        return wish_wrapper
+    return wish_decorator
--- specs/test_object_from_name.py
+++ specs/test_object_from_name.py
@@ -1,19 +1,19 @@
 
 import os
 
+import pytest_nodev
 
-def test_object_from_name_simple(wish):
-    object_from_name = wish
 
+@pytest_nodev.search('object_from_name')
+def test_object_from_name_simple():
     assert object_from_name('os:O_CREAT') is os.O_CREAT
     assert object_from_name('os.path:join') is os.path.join
     assert object_from_name('builtins:True') is True
     assert object_from_name('builtins:open') is open
 
 
-def test_object_from_name_pep3155(wish):
-    object_from_name = wish
-
+@pytest_nodev.search('object_from_name')
+def test_object_from_name_pep3155():
     # instance methods compare by equality, see http://stackoverflow.com/questions/15977808
     assert object_from_name('os:O_CREAT.bit_length') == os.O_CREAT.bit_length
     assert object_from_name('builtins:int.bit_length') is int.bit_length
